ff28565961687a4319d7355e180d1187efdaf0cd
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2018-03-17 20:24:58 +0000

    [add] Basic jira auth in config file

--- README.md
+++ README.md
@@ -8,5 +8,12 @@ pip install -r requirements.txt
 
 # Configuring the enviroment
 ```sh
-python setup.py
+python setup.py --baseUrl <jira url> --userName <jira username> --apiKey <jira api key>
 ```
+
+# Parameters
+|   Option   | Type   | Value                                                                   |
+|:----------:|--------|-------------------------------------------------------------------------|
+| --baseUrl  | String | The url to acces the jira project: example.com:8080 or domain.jira.net  |              |
+| --userName | String | The user name used to login                                             |
+| --apiKey   | String | The generated api key from the jira interface, will be used as password |
--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
+import argparse
 import configparser
-config = configparser.ConfigParser()
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--baseUrl', type=str, help='The base url for the jira project')
+parser.add_argument('--userName', type=str, help='The user name to use for authentication')
+parser.add_argument('--apiKey', type=str, help='The api key for jira, will be used instead of a password')
+args = parser.parse_args()
+
+
+config = configparser.ConfigParser()
 config['jira'] = {}
-config['jira']['baseUrl'] = ''
+config['jira']['baseUrl'] = args.baseUrl
+config['jira']['userName'] = args.userName
+config['jira']['apiKey'] = args.apiKey
 
 with open('config.ini', 'w') as configfile:
   config.write(configfile)
