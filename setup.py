1557b37da34787b05f70b3df9ad318b2b62e7672
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2018-03-17 21:47:45 +0000

    [mod] Config file restructured

--- setup.py
+++ setup.py
@@ -7,18 +7,25 @@ parser.add_argument('--userName', type=str, help='The user name to use for authe
 parser.add_argument('--apiKey', type=str, help='The api key for jira, will be used instead of a password')
 args = parser.parse_args()
 
+#print(args)
 
 config = configparser.ConfigParser()
-config['jira'] = {}
-config['jira']['baseUrl'] = args.baseUrl
-config['jira']['apiPartUrl'] = "/rest" # This is what follows the jira url
-config['jira']['listsBoardsUrlPart'] = "agile/1.0/board"
-config['jira']['listsIssuesByBoardPart1Url'] = "/agile/1.0/board/"
-config['jira']['listsIssuesByBoardPart2Url'] = "/issue"
-config['jira']['metadataForIssues'] = "/api/2/issue/createmeta"
-config['jira']['sendIssueUrl'] = "/api/2/issue/"
-config['jira']['userName'] = args.userName
-config['jira']['apiKey'] = args.apiKey
+config['auth'] = {}
+config['auth']['userName'] = args.userName
+config['auth']['apiKey'] = args.apiKey
+config['url'] = {}
+config['url']['baseUrl'] = args.baseUrl # The jira url
+config['url']['apiPartUrl'] = '/rest' # This is what follows the jira url
+config['boardListing'] = {}
+config['boardListing']['urlPart'] = '/agile/1.0/board'
+config['issuesByBoard'] = {}
+# <baseUrl>/rest/agile/1.0/<boardId>/293/issue 
+config['issuesByBoard']['urlPart1'] = '/agile/1.0/board/'
+config['issuesByBoard']['urlPart2'] = '/issue'
+config['metadataForIssues'] = {}
+config['metadataForIssues']['urlPart'] = '/api/2/issue/createmeta'
+config['sendIssue'] = {}
+config['sendIssue']['urlPart'] = '/api/2/issue'
 
 with open('config.ini', 'w') as configfile:
   config.write(configfile)
