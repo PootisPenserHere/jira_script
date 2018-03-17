dc81e82e4f18d671659e9c59cabf54cf6fd5269d
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2018-03-17 21:55:14 +0000

    [add] Request type
    Also added spacing between the different configurations and removed some commented code

--- setup.py
+++ setup.py
@@ -7,25 +7,32 @@ parser.add_argument('--userName', type=str, help='The user name to use for authe
 parser.add_argument('--apiKey', type=str, help='The api key for jira, will be used instead of a password')
 args = parser.parse_args()
 
-#print(args)
-
 config = configparser.ConfigParser()
 config['auth'] = {}
 config['auth']['userName'] = args.userName
 config['auth']['apiKey'] = args.apiKey
+
 config['url'] = {}
 config['url']['baseUrl'] = args.baseUrl # The jira url
 config['url']['apiPartUrl'] = '/rest' # This is what follows the jira url
+
 config['boardListing'] = {}
 config['boardListing']['urlPart'] = '/agile/1.0/board'
-config['issuesByBoard'] = {}
+config['boardListing']['requestType'] = 'get'
+
 # <baseUrl>/rest/agile/1.0/<boardId>/293/issue 
+config['issuesByBoard'] = {}
 config['issuesByBoard']['urlPart1'] = '/agile/1.0/board/'
 config['issuesByBoard']['urlPart2'] = '/issue'
+config['issuesByBoard']['requestType'] = 'get'
+
 config['metadataForIssues'] = {}
 config['metadataForIssues']['urlPart'] = '/api/2/issue/createmeta'
+config['metadataForIssues']['requestType'] = 'get'
+
 config['sendIssue'] = {}
 config['sendIssue']['urlPart'] = '/api/2/issue'
+config['sendIssue']['requestType'] = 'post'
 
 with open('config.ini', 'w') as configfile:
   config.write(configfile)
