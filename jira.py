b4dc8e9c2dcb48a2b12bf4109d5a27b013555d31
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2018-03-18 05:31:41 +0000

    [add] Requester moved to its own class

--- jira.py
+++ jira.py
@@ -1,34 +1,7 @@
-""" Wrapper for the different jira endpoints that will be used as well 
-as container for the interaction logic
-"""
-class jira(object):
-    def __init__(self):
-        import configparser
-        import requests
-        
-        # To do http requests
-        self.requests = requests
-        
-        # Data parsed from the config file
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-        
-        self.baseUrl = config['url']['baseurl']
-        self.apiparturl = config['url']['apiparturl']
-        
-        self.userName = config['auth']['userName']
-        self.apikey = config['auth']['apikey']
-        
-        self.boardListingUrl = "%s%s%s" % (self.baseUrl, self.apiparturl, config['boardListing']['urlpart'])
-        self.boardListingRequestType = config['boardListing']['requesttype']
-        
-    def listBoards(self):
-        return self.requests.get(self.boardListingUrl, auth=(self.userName, self.apikey))
-
 """ Abstracts the process of converting the output from from the requests 
 into more workable json objects
 """
-class outputHandler():
+class OutputHandler(object):
     def __init__(self):
         import json
         
@@ -39,13 +12,55 @@ class outputHandler():
         data = self.json.loads(data)
         return self.json.dumps(data, sort_keys=True, indent=4)
 
+""" To make the requests to the jira plataform and return their raw 
+output 
+
+@param user string - the user name to sign into the jira plataform
+@param password string - the api key or password for the authentication
+"""
+class Requester():
+    def __init__(self, user, password):
+        import requests
+        
+        self.requests = requests
+        self.user = user
+        self.password = password
+        
+    def generalPurpose(self, url):
+        return self.requests.get(url, auth=(self.user, self.password))
+        
+        
+""" Wrapper for the different jira endpoints that will be used as well 
+as container for the interaction logic
+"""
+class Jira(object):
+    def __init__(self, Requester):
+        import configparser
+        
+        # Data parsed from the config file
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        
+        self.userName = config['auth']['userName']
+        self.apikey = config['auth']['apikey']
+        self.requester = Requester(self.userName, self.apikey)
+        
+        self.baseUrl = config['url']['baseurl']
+        self.apiparturl = config['url']['apiparturl']
+        
+        self.boardListingUrl = "%s%s%s" % (self.baseUrl, self.apiparturl, config['boardListing']['urlpart'])
+        self.boardListingRequestType = config['boardListing']['requesttype']
+        
+    def listBoards(self):
+        return self.requester.generalPurpose(self.boardListingUrl)
+
 """ The application logic 
 
 The classes above will be used here to serve the requests made to the 
 script
 """
-jira = jira()
-outputHandler = outputHandler()
+jira = Jira(Requester)
+outputHandler = OutputHandler()
 
 output = jira.listBoards()
 print outputHandler.outputToJson(output)
